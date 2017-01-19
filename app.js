e3eb01e5d119447ccb8d90bd8a4576f2e2bb927a
Steve <stevekreider7@gmail.com>
2017-01-18 21:45:31 -0500

    prompts with states

--- app.js
+++ app.js
@@ -25,45 +25,73 @@ let app = express();
 app.set('port', (process.env.PORT || 8081));
 app.use(bodyParser.json({type: 'application/json'}));
 
+
+
+function mainIntent (assistant) {
+	console.log('mainIntent: '+ assistant);
+	let inputPrompt = assistant.buildInputPrompt(true, 
+			'What type of JIRA issue would you like to create?',
+			['I didn\'t hear an issue type']);
+	assistant.ask(inputPrompt,{'state':'getIssueType','data':{}});
+}
+
+function getIssueType(assistant){
+	let input = assistant.getRawInput();
+	
+	let inputPrompt = assistant.buildInputPrompt(true, 
+			'What would you like the summary of your ' + input +' to be',
+			['Huh?']);
+	let dialogueState = assistant.getDialogState();
+	dialogueState.data.issueType = input;
+	dialogueState.state = 'getSummary';
+	
+	
+	if(input==='story'){
+		assistant.ask(inputPrompt, dialogueState);
+	}else if(input==='task'){
+		assistant.ask(inputPrompt, dialogueState);
+	}
+}
+
+function rawInput (assistant) {
+	console.log('rawInputIssueType: \'' +assistant.getRawInput()+'\'');
+	let dialogueState = assistant.getDialogState();
+	console.log('state: \''+ JSON.stringify(dialogueState) +'\'')
+	
+	switch(dialogueState.state){
+		case 'getIssueType':
+			getIssueType(assistant);
+			break;
+		
+	}
+	
+	/*if(state.state === 'getIssueType' && assistant.getRawInput() === 'story'){
+		state.state = ''
+		assistant.tell('Creating a story');
+	} else {
+		let inputPrompt = assistant.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">' +
+				assistant.getRawInput() + '</say-as></speak>',
+				['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
+		assistant.ask(inputPrompt);
+	}*/
+}
+
 app.post('/', function (request, response) {
-  console.log('handle post');
-  const assistant = new ActionsSdkAssistant({request: request, response: response});
+	console.log('handle post');
+	const assistant = new ActionsSdkAssistant({request: request, response: response});
 
-  function mainIntent (assistant) {
-    console.log('mainIntent');
-    let inputPrompt = assistant.buildInputPrompt(true, '<speak>Hi! <break time="1"/> ' +
-          'I can read out an ordinal like ' +
-          '<say-as interpret-as="ordinal">123</say-as>. Say a number.</speak>',
-          ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
-    assistant.ask(inputPrompt);
-  }
 
-  function altIntent(assistant){
-    console.log('altIntent')
-  }
 
-  function rawInput (assistant) {
-    console.log('rawInput');
-    if (assistant.getRawInput() === 'bye') {
-      assistant.tell('Goodbye!');
-    } else {
-      let inputPrompt = assistant.buildInputPrompt(true, '<speak>You said, <say-as interpret-as="ordinal">' +
-        assistant.getRawInput() + '</say-as></speak>',
-          ['I didn\'t hear a number', 'If you\'re still there, what\'s the number?', 'What is the number?']);
-      assistant.ask(inputPrompt);
-    }
-  }
+	let actionMap = new Map();
+	actionMap.set(assistant.StandardIntents.MAIN, mainIntent);
+	actionMap.set(assistant.StandardIntents.TEXT, rawInput);
 
-  let actionMap = new Map();
-  actionMap.set(assistant.StandardIntents.MAIN, mainIntent);
-  actionMap.set(assistant.StandardIntents.TEXT, rawInput);
-
-  assistant.handleRequest(actionMap);
+	assistant.handleRequest(actionMap);
 });
 
-// Start the server
+//Start the server
 let server = app.listen(app.get('port'), function () {
-  console.log('App listening on port %s', server.address().port);
-  console.log('Press Ctrl+C to quit.');
+	console.log('App listening on port %s', server.address().port);
+	console.log('Press Ctrl+C to quit.');
 });
-// [END app]
+//[END app]
